0ed7234018f2d90dc42df8933ce2f133e39475d7
VitorAlex-97 <vitualex97@gmail.com>
2023-04-01 20:05:57 -0300

    Feature: Adicioando FlatList para renderização do Card de produtos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,49 @@
 import { Box } from "native-base";
 import { DetailBackground } from "../../atomic/atoms/DetailBackgroun";
 import { MainBanner } from "../../atomic/molecules/MainBanner";
+import { Card } from "../../atomic/molecules/Card";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { FlatList } from "react-native";
 
+export type CardProps = {
+  id: string;
+  title: string;
+  price: number;
+  model: string;
+  image: string;
+}
 export function Home() {
+
+  const [equipments, setEquipments] = useState<CardProps[]>([])
+
+  useEffect(() => {
+    
+    async function getEquipaments() {
+      try {
+        const { data } = await api.get('equipments');
+        setEquipments(data)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getEquipaments()
+
+  }, [])
+
   return (
     <Box flex='1' padding='20px' pt='-10px' position='relative'>
       <DetailBackground />
-      <MainBanner />
+      <FlatList
+        ListHeaderComponent={() => <MainBanner />}
+        keyExtractor={item => item.id}
+        data={equipments}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        renderItem={({ item: equipment }) => <Card title={equipment.title} image={equipment.image} id={equipment.id} price={equipment.price} model={equipment.model}/>}
+      />
     </Box>
   )
 }
