6639fc4aa6090fc4dd89fd59616f8f863fa4a47a
VitorAlex-97 <vitualex97@gmail.com>
2023-04-01 18:22:26 -0300

    Add: Adicionando Molecula MainBanner

---
+++ src/atomic/molecules/MainBanner.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, Image } from 'native-base';
+import { ImageBackground } from 'react-native';
+
+import slider from '../../img/slider.png';
+import bike from '../../img/bike.png';
+
+export function MainBanner() {
+  return (
+    <Box
+      w='100%'
+      h='300'
+      mt='60'
+    >
+      <ImageBackground
+        source={slider}
+        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        resizeMode='contain'
+      >
+        <Image 
+          source={bike}
+          alt='image of bike'
+        />
+      </ImageBackground>
+    </Box>
+  )
+}
