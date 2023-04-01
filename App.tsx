cb72a91d9a1c3bdae9559dc4afec2e31c0a0f5a5
VitorAlex-97 <vitualex97@gmail.com>
2023-04-01 18:21:14 -0300

    Add: Adicionando DetailBackground

--- App.tsx
+++ App.tsx
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { Box, NativeBaseProvider } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
 import { theme } from './src/styles/theme';
+import { Home } from './src/pages/Home';
 
 export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
       <Box flex='1' bg={theme.colors.primary[100]}>
-        
+        <Home />
       </Box>
     </NativeBaseProvider>
   );
