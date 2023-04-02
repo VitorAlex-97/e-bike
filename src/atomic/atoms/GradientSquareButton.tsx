57251007dc24f64e79fa1ffe6215df7dd782dfa3
VitorAlex-97 <vitualex97@gmail.com>
2023-04-02 11:24:28 -0300

    Feature: Adicionando atomo GradientSquareButton

---
+++ src/atomic/atoms/GradientSquareButton.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Box, useTheme } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export function GradientSquareButton({
+  children
+}: Props) {
+  const { colors } = useTheme()
+  return (
+    <TouchableOpacity
+      
+    >
+      <Box shadow='5'>
+        <LinearGradient
+          colors={[colors.ocean[100], colors.purple[100] ]}
+          style={{ width: 44, height: 44, justifyContent: 'center',  alignItems: 'center', borderRadius: 10 }}
+        >
+          {children}
+        </LinearGradient>
+      </Box>
+    </TouchableOpacity>
+  )
+}
